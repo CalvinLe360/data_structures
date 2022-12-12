0b83cbf7ca680689f92b93eb5ae983a73ffeacd8
Snoresville <calvinle360@gmail.com>
2022-12-12 21:25:32 +1030

    BFS and DFS

--- adjacencyList.py
+++ adjacencyList.py
@@ -1,3 +1,6 @@
+import random
+
+
 class AdjacencyList:
     def __init__(self):
         self.nodeUID = 0
@@ -18,6 +21,9 @@ class AdjacencyList:
         self.nodeGraph.pop(node)
 
     def addEdge(self, node1, node2):
+        if node1 == node2:
+            return
+
         if node1 not in self.nodeGraph:
             print(str(node1) + " is not in the graph!")
             return
@@ -56,17 +62,82 @@ class AdjacencyList:
         
         searchDepth = 0
         while len(queue) > 0:
-            searching = queue
+            searching.extend(queue)
             queue = []
+
+            print("Current Search Depth: " + str(searchDepth))
+            print("Elements In Depth: ", searching)
+            print("Elements Explored: ", explored)
+            print("---")
+
+            while len(searching) > 0:
+                exploring = searching.pop()
+                explored.append(exploring)
+
+                if exploring == end:
+                    return True
+
+                for node in self.nodeGraph[exploring]:
+                    if node in explored or node in searching or node in queue:
+                        continue
+                    
+                    queue.append(node)
+
+            searchDepth += 1
+
+        return False
+    
+    def dfs(self, start, end):
+        explored = []
+        stack = [start]
+        
+        while len(stack) > 0:
+            searchingNode = stack[-1]
+
+            print("Search Chain: " + ' -> '.join(stack))
+            print("Explorable Nodes: ", self.nodeGraph[searchingNode])
+            print("Fully Explored: ", explored)
+            print("---")
+
+            if searchingNode == end:
+                return True
+
+            for node in self.nodeGraph[searchingNode]:
+                if node not in explored and node not in stack:
+                    stack.append(node)
+                    break
+
+            if stack[-1] == searchingNode:
+                explored.append(stack.pop())
+
+        return False
+
+    def randomGen(self):
+        for _ in range(10):
+            self.addNode()
+
+        for _ in range(2):
+            nodeSet1 = [_ for _ in self.nodeGraph.keys()]
+            nodeSet2 = [_ for _ in self.nodeGraph.keys()]
+            random.shuffle(nodeSet1)
+
+            for x, y in zip(nodeSet1, nodeSet2):
+                self.addEdge(x, y)
+
+
+
 
 
 
 instructions = [
     "? - Read Instructions Again",
     "new - Adds a new vertex to the graph",
+    "random - Randomly generates ten nodes and adds edges to every node"
     "remove x - Deletes a vertex with the specified id",
     "add x y - Adds an edge between two vertices",
     "destroy x y - Removes an edge between two vertices",
+    "bfs x y - Searches for y from x with Breadth First Search",
+    "dfs x y - Searches for y from x with Depth First Search",
     "print - Prints the entire linked list",
     "quit - Closes the program",
 ]
@@ -86,6 +157,9 @@ def main():
         elif userInput[0] == "new":
             adjList.addNode()
             adjList.printGraph()
+        elif userInput[0] == "random":
+            adjList.randomGen()
+            adjList.printGraph()
         elif userInput[0] == "print":
             adjList.printGraph()
         elif userInput[0] == "quit":
@@ -101,6 +175,16 @@ def main():
                 elif userInput[0] == "destroy":
                     adjList.removeEdge(userInput[1], userInput[2])
                     adjList.printGraph()
+                elif userInput[0] == "bfs":
+                    if adjList.bfs(userInput[1], userInput[2]):
+                        print(userInput[2] + " found with BFS starting from " + userInput[1])
+                    else:
+                        print(userInput[2] + " cannot be found starting from " + userInput[1])
+                elif userInput[0] == "dfs":
+                    if adjList.dfs(userInput[1], userInput[2]):
+                        print(userInput[2] + " found with DFS starting from " + userInput[1])
+                    else:
+                        print(userInput[2] + " cannot be found starting from " + userInput[1])
                 else:
                     print("Invalid input")
             else:
--- binaryTree.py
+++ binaryTree.py
@@ -1,8 +1,8 @@
 class BinaryTreeNode:
     def __init__(self, data):
         self.data = int(data)
-        self.leftPointer: BinaryTreeNode = None
-        self.rightPointer: BinaryTreeNode = None
+        self.left: BinaryTreeNode = None
+        self.right: BinaryTreeNode = None
 
 class BinaryTree:
     def __init__(self, root: BinaryTreeNode):
@@ -16,24 +16,36 @@ class BinaryTree:
         target = self.root
         while True:
             if node.data < target.data:
-                if target.leftPointer is None:
-                    target.leftPointer = node
+                if target.left is None:
+                    target.left = node
                     return
                 else:
-                    target = target.leftPointer
+                    target = target.left
             else:
-                if target.rightPointer is None:
-                    target.rightPointer = node
+                if target.right is None:
+                    target.right = node
                     return
                 else:
-                    target = target.rightPointer
+                    target = target.right
 
-    def deleteNode(self, value: int):
-        grandparent = self.root
+instructions = [
+    "? - Read Instructions Again",
+    "new - Adds a new vertex to the graph",
+    "random - Randomly generates ten nodes and adds edges to every node"
+    "remove x - Deletes a vertex with the specified id",
+    "add x y - Adds an edge between two vertices",
+    "destroy x y - Removes an edge between two vertices",
+    "bfs x y - Searches for y from x with Breadth First Search",
+    "dfs x y - Searches for y from x with Depth First Search",
+    "print - Prints the entire linked list",
+    "quit - Closes the program",
+]
 
-        # if grandparent.data == value:
+def main():
+    binaryTree = BinaryTree()
 
+    for i in instructions:
+        print(i)
 
-        # while grandparent is not None:
-        #     if grandparent.data == value:
-        #         if grandparent.leftPointer is None and grandparent.rightPointer is not None:
+    while True:
+        print()
